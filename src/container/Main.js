73b6afd88b989b754162909f04dc4809f6fba2c1
sumin_hong <sumiiniii@ajou.ac.kr>
2020-08-25 17:09:58 +0900

    making main page 1

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,5 +1,7 @@
 import React from "react";
+import axios from 'axios';
 //컨테이너 역할을 하는 컴포넌트들은 prop를 router를 통해 전달 받을 예정이므로 class형으로 만든다.
+// setState()는 컴포넌트의 state객체에 대한 업데이트를 실행하고 state가 변경되면 컴포넌트는 리렌더링된다.
 
 import Header from "../component/Header";
 import Gnb from "../component/Gnb";
@@ -8,11 +10,31 @@ import Footer from "../component/Footer";
 class Main extends React.Component{
     constructor(props){
         super(props);
-
+        
         this.state={
             day:'mon',
             webtoonList : []
         };
+    }
+
+    componentDidMount(){
+        this._getList();
+    }
+    _getList(){
+        const apiUrl = 'dummy/webtoon_list.json';
+
+        axios.get(apiUrl)
+            .then(function (data ) {
+                this.setState({
+                    webtoonList:data.data.webtoonList
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        
+
     }
     
     render(){
@@ -20,7 +42,22 @@ class Main extends React.Component{
             <div>
                 <Header/>
                 <Gnb />
-                Main
+                {
+                    this.state.webtoonList.length > 0? 
+                    (
+                        <webtoonList list={
+                            this.state.webtoonList.filter(webtoon => (
+                                //filter는 array의 모든 아이템을 통해 함수를 실행하고 true인 아이템들만 가지고 새로운 array만든다.
+                                webtoon.day === this.state.day //웹툰 리스트 중 요일에 해당하는 웹툰만 반환
+                            ))
+                        } />
+                    ) 
+                    : 
+                    (
+                        <span>
+                            Loading...
+                        </span>
+                    )}
                 <Footer />
             </div>
             
@@ -29,3 +66,10 @@ class Main extends React.Component{
 }
 
 export default Main;
+
+
+/*
+동기적 일처리 방식 : 순차적으로 일을 스스로 끝내 나가는 방식
+비동기적 일처리 방식 : 해야 할 일을 위임하고 기다리는 방식
+
+*/
