3be4c1b0506416cdb4c2603aa9ae8bfb5c6d2bbe
sumin_hong <sumiiniii@ajou.ac.kr>
2020-08-27 16:40:31 +0900

    making viewer 1

--- src/container/Viewer.js
+++ src/container/Viewer.js
@@ -1,9 +1,48 @@
 import React from "react";
+import axios from "axios";
 
 class Viewr extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            episodeId:parseInt(props.match.params.episodeId,10),
+            episode :{}
+        };
+    }
+
+    componentDidMount(){
+        this._getEpisodeList();
+    }
+
+    _getEpisodeList(){
+        const apiUrl = '/dummy/episode_list.json';
+
+        axios.get(apiUrl)
+            .then(data => {
+                this.setState({
+                    episode : data.data.webtoonEpisodes.find(episode => (
+                        episode.id===this.state.episodeId
+                    ))
+                });
+            })
+
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+
+
     render(){
         return(
-            <div>Viewer</div>
+            <div className="wrap_viewer">
+                {episode.title}
+                <Link to={`/webtoon/${episode.webtoonId}`} className="btn_cloase">
+
+                </Link>
+
+            </div>
         )
     }
 }
